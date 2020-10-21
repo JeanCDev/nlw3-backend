7349e07a076ea829657134293b61a3c0aac46f19
JeanCDev <jeangames15@gmail.com>
2020-10-21 10:12:53 -0300

    deploy

--- src/server.ts
+++ src/server.ts
@@ -9,12 +9,17 @@ import './database/connection';
 import routes from './routes';
 import errorHandler from './errors/handler';
 
+var corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200
+}
+
 const app = express();
 
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.use(errorHandler);
 
