9e65033ce72b5ccdb474f630a4898a6359a935bc
JeanCDev <jeangames15@gmail.com>
2020-10-21 10:18:35 -0300

    deploy

--- src/server.ts
+++ src/server.ts
@@ -10,16 +10,16 @@ import routes from './routes';
 import errorHandler from './errors/handler';
 
 var corsOptions = {
-  origin: 'https://nlw-3-happy.netlify.app',
+  origin: '*',
   optionsSuccessStatus: 200
 }
 
 const app = express();
 
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
-app.use(cors(corsOptions));
 
 app.use(errorHandler);
 
